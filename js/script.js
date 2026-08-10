985a4b382b076a607f790950f488017f7fb7b420
Luka <parezanovicluka1999@gmail.com>
2026-08-10 18:03:25 +0200

    Automatska izmena sajta

--- js/script.js
+++ js/script.js
@@ -1,21 +1,5 @@
 function toggleMenu() {
     const nav = document.getElementById("main-nav");
-
-    nav.classList.toggle("active");
-}
-
-function promeniTekst() {
-    alert("Dobrodošao na sajt FK Propast!");
-}
-
-const countdown = document.getElementById("countdown");
-
-countdown.innerHTML = "TEST";
-
-alert("JavaScript radi!");
-
-function toggleMenu() {
-    const nav = document.getElementById("main-nav");
     nav.classList.toggle("active");
 }
 
